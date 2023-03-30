37e0c0a0f05f28019a71559dfeddafcd18b52f0b
Stevan Leonardy <stevan11220@gmail.com>
2023-03-30 16:58:54 +0700

    change code

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -6,17 +6,17 @@ the calc.py library's 'add2' function.
 import sys
 import calc
 
-argnumbers = len(sys.argv) - 1
+argnums = len(sys.argv) - 1
 
-if argnumbers == 2 :
+if argnums == 2 :
     print("")
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
     print("")
     sys.exit(0)
 
-if argnumbers != 2 :
+if argnums != 2 :
     print("")
-    print("You entered " + str(argnumbers) + " value/s.")
+    print("You entered " + str(argnums) + " value/s.")
     print("")
     print("Usage: 'add2vals X Y' where X and Y are individual values.")
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
